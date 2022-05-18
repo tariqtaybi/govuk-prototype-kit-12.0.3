1fefb31eba302e07d380e9eaae1fd483cba6ed51
Tariq Taybi <33510650+tariqtaybi@users.noreply.github.com>
2022-05-18 22:13:58 +0100

    IMT

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a juggling licence',
+  serviceName: 'Inclusive Modelling Tool',
 
   // Default port that prototype runs on
   port: '3000',
